7a3a74f031cc59e70587fe7149533aa774a98238
Piotr Pikuła <ppikula@future-processing.com>
2015-04-06 20:00:26 +0200

    move class Sorter to other file

--- src/rfplugin/views/SampleView.java
+++ src/rfplugin/views/SampleView.java
@@ -88,21 +88,6 @@ public class SampleView extends ViewPart {
 		});
 	}
 
-	class Sorter extends ViewerSorter {
-		public int compare(Viewer viewer, Object e1, Object e2) {
-			if (e1 instanceof String && e2 instanceof String) {
-				String g1 = (String) e1;
-				String g2 = (String) e2;
-
-				if (g1 == "Not run")
-					return 1;
-
-				return g1.compareTo(g2);
-			}
-			return 0;
-		}
-	}
-
 	public class StreamWrapper extends Thread {
 		InputStream inputStream = null;
 		Test actualRunTest = null;
